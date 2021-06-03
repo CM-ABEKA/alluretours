6427e2b7698a58e9af1f0c581db0fcaf5a6520a5
CM-ABEKA <cmabeka2016@gmail.com>
2021-06-03 21:13:30 +0300

    u

--- dbConnect.php
+++ dbConnect.php
@@ -1,19 +1,62 @@
 <?php 
 
 try{
-    $myPDO = new PDO("pgsql:host=ec2-52-19-164-214.eu-west-1.compute.amazonaws.com;dbname=d7skoghl2kgf0q", "uurjvqciualsjp", "87fb850b56b702e037ee86dbc0d6583b5a47062600277a87e6cf55e60d413283");
+    $pdo = new PDO("pgsql:host=ec2-52-19-164-214.eu-west-1.compute.amazonaws.com;dbname=d7skoghl2kgf0q", "uurjvqciualsjp", "87fb850b56b702e037ee86dbc0d6583b5a47062600277a87e6cf55e60d413283");
     echo 'connected to postgresql with pdo';
+
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e){
     echo $e->getMessage();
 }
-
-$sql = SELECT * FROM users;
-$stmt = $myPDO->prepare($sql);
-$stmt -> excecute();
-$rowcount = $stmt->rowCount();
-$details = $stmt->fetch();
-
-print_r ($details);
-
 ?>
+
+<html>
+
+<head>
+    <title>Heroku Test</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Heroku Test</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li>
+                    <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="<?php echo ROOT_URL; ?>about">About</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="<?php echo ROOT_URL; ?>contact">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <?php
+  //echo 'This is Index Page';
+
+  $sql = 'SELECT * FROM users';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $rowCount = $stmt->rowCount();
+  $details = $stmt->fetch();
+
+  print_r ($details);
+?>
+
+</body>
+
+</html>
